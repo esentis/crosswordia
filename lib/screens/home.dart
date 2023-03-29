883aff33315e52a1128aed031cea796b514a9236
esentis <52862600+esentis@users.noreply.github.com>
2023-03-30 00:37:58 +0300

    🤡 Adds mocks

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:crosswordia/providers/auth_provider.dart';
 import 'package:crosswordia/screens/login.dart';
+import 'package:crosswordia/services/grouped_words_service.dart';
 import 'package:crosswordia/services/player_status_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,46 +15,61 @@ class HomeScreen extends StatelessWidget {
         builder: (context, status, child) {
           print('Current state is ${status.isAuthenticated}');
           if (status.isAuthenticated) {
-            return Column(
-              children: [
-                Center(
-                  child: const Text('Welcome you are logged in'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    PlayerStatusService.instance
-                        .getPlayerStatus(status.session!.user.id);
-                  },
-                  child: const Text('Get status'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    PlayerStatusService.instance
-                        .incrementTotalCoins(status.session!.user.id, 100);
-                  },
-                  child: const Text('Add coins'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    PlayerStatusService.instance
-                        .incrementTotalWordsFound(status.session!.user.id, 25);
-                  },
-                  child: const Text('Add words'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    PlayerStatusService.instance
-                        .incrementLevel(status.session!.user.id);
-                  },
-                  child: const Text('Increment level'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    status.signOut();
-                  },
-                  child: const Text('Logout'),
-                ),
-              ],
+            return SafeArea(
+              child: Column(
+                children: [
+                  const Center(
+                    child: Text('Welcome you are logged in'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      PlayerStatusService.instance
+                          .getPlayerStatus(status.session!.user.id);
+                    },
+                    child: const Text('Get status'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      PlayerStatusService.instance
+                          .incrementTotalCoins(status.session!.user.id, 100);
+                    },
+                    child: const Text('Add coins'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      PlayerStatusService.instance.incrementTotalWordsFound(
+                          status.session!.user.id, 25);
+                    },
+                    child: const Text('Increment total words'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      PlayerStatusService.instance.updateFoundWords(
+                          status.session!.user.id, ['hello', 'world', 'test']);
+                    },
+                    child: const Text('Update current level words'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      PlayerStatusService.instance
+                          .incrementLevel(status.session!.user.id);
+                    },
+                    child: const Text('Increment level'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      GroupedWordsService.instance.addGroupedWordsFromMap();
+                    },
+                    child: const Text('Add grouped words in DB'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      status.signOut();
+                    },
+                    child: const Text('Logout'),
+                  ),
+                ],
+              ),
             );
           }
           return const LoginScreen();
