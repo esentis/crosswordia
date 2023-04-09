b25df0d54d1b7a7257e08bd38592e19587a3cf67
esentis <52862600+esentis@users.noreply.github.com>
2023-04-09 05:29:55 +0300

    👽️ Adds new api methods

--- lib/services/player_status_service.dart
+++ lib/services/player_status_service.dart
@@ -99,18 +99,142 @@ class PlayerStatusService {
     }
   }
 
-  Future<void> updateLevelsProgress(
-      String playerId, List<Map<String, dynamic>> levelsProgress) async {
-    kLog.i('Updating found words for $playerId');
+  /// Checks if the level progress exists for the player
+  ///
+  /// If it does not exist, it will be initialized
+  Future<void> checkIfLevelProgressExists(String playerId, int level) async {
+    kLog.i('Checking if level progress exists for $playerId');
+    final levelId = await getLevelId(level);
+
+    if (levelId == null) {
+      kLog.e('Level id is null');
+      return;
+    }
     try {
-      final data = await Supabase.instance.client.from('player_status').update({
-        'levels_progress': levelsProgress,
-      }).eq('player_id', playerId);
+      final data = await Supabase.instance.client
+          .from('player_level_status')
+          .select()
+          .eq('player_id', playerId)
+          .eq('level_id', levelId);
 
       kLog.wtf(data);
+      if (data.isEmpty) {
+        await initLevelProgress(playerId, level);
+      }
+    } on PostgrestException catch (e) {
+      kLog.e(e.message);
+      if (e.message.contains('does not exist')) {
+        await initLevelProgress(playerId, level);
+      }
+    }
+  }
+
+  /// Updates the level progress for the player
+  ///
+  /// If the level progress does not exist, it will be initialized
+  Future<void> updateLevelProgress(
+      String playerId, int level, List<String> words) async {
+    kLog.i('Updating found words for $playerId');
+
+    final levelId = await getLevelId(level);
+
+    if (levelId == null) {
+      kLog.e('Level id is null');
+      return;
+    }
+    await checkIfLevelProgressExists(playerId, level);
+
+    try {
+      final data = await Supabase.instance.client
+          .from('player_level_status')
+          .update({
+            'found_words': words,
+          })
+          .eq('player_id', playerId)
+          .eq('level_id', levelId);
+
+      kLog.wtf(data);
+    } on PostgrestException catch (e) {
+      kLog.e(e.message);
+      if (e.message.contains('does not exist')) {
+        await initLevelProgress(playerId, level);
+        await updateLevelProgress(playerId, level, words);
+      }
+    }
+  }
+
+  /// Adds a word to the level progress for the player
+  Future<void> addWordInLevelProgress(
+      String playerId, int level, String word) async {
+    kLog.i('Adding word in level progress for $playerId');
+
+    final levelId = await getLevelId(level);
+
+    if (levelId == null) {
+      kLog.e('Level id is null');
+      return;
+    }
+    await checkIfLevelProgressExists(playerId, level);
+
+    try {
+      final data =
+          await Supabase.instance.client.rpc('updatewordlevel', params: {
+        'player': playerId,
+        'word': word,
+        'level': levelId,
+      });
+
+      kLog.wtf(data);
+    } on PostgrestException catch (e) {
+      kLog.e(e.message);
+      if (e.message.contains('does not exist')) {
+        await initLevelProgress(playerId, level);
+        await updateLevelProgress(playerId, level, [word]);
+      }
+    }
+  }
+
+  /// Initializes the level progress for the player
+  Future<void> initLevelProgress(String playerId, int level) async {
+    kLog.i('Initializing level progress for $playerId');
+    final int? levelId = await getLevelId(level);
+
+    if (levelId == null) {
+      kLog.e('Level id is null');
+      return;
+    }
+    try {
+      final data =
+          await Supabase.instance.client.from('player_level_status').insert({
+        'player_id': playerId,
+        'level_id': levelId,
+        'found_words': [],
+      });
+
+      kLog.wtf(data);
+    } on PostgrestException catch (e) {
+      kLog.e(e.message);
+    }
+  }
+
+  /// Gets the level id from the level table
+  Future<int?> getLevelId(int level) async {
+    kLog.i('Getting level id for $level');
+    try {
+      final data = await Supabase.instance.client
+          .from('levels')
+          .select()
+          .eq('level', level);
+
+      kLog.wtf('Level id is ${data[0]['id']}');
+      if (data.isNotEmpty) {
+        return data[0]['id'];
+      }
     } catch (e) {
       kLog.e(e);
+      return null;
     }
+    return null;
   }
 }
 
