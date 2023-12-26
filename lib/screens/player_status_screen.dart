12657c362a07526cde5eaa0218394722817fc3a1
esentis <52862600+esentis@users.noreply.github.com>
2023-12-26 22:57:51 +0200

    Add PlayerStatusScreen widget

---
+++ lib/screens/player_status_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlayerStatusScreen extends StatefulWidget {
+  const PlayerStatusScreen({super.key});
+
+  @override
+  State<PlayerStatusScreen> createState() => _PlayerStatusScreenState();
+}
+
+class _PlayerStatusScreenState extends State<PlayerStatusScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
