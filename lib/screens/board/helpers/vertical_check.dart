dd0b2aea5eec264fe9c57c806ebb58fa580dddef
esentis <52862600+esentis@users.noreply.github.com>
2025-05-14 23:38:53 +0300

    ♻️ Refactor canStartVertically function to use WordPlacementData model for improved readability and maintainability

--- lib/screens/board/helpers/vertical_check.dart
+++ lib/screens/board/helpers/vertical_check.dart
@@ -1,33 +1,16 @@
 import 'package:crosswordia/extensions/map_extensions.dart';
 import 'package:crosswordia/scraper.dart';
+import 'package:crosswordia/services/models/word_placement_data.dart';
 import 'package:string_extensions/string_extensions.dart';
 
-bool canStartVertically({
-  required int distanceFromTopOfLetter,
-  required int distanceFromBottomtOfLetter,
-  required int spaceFromTop,
-  required int spaceFromBottom,
-  required String actualVerticalStartingLocationIfAvailable,
-  required String actualVerticalEndingLocationIfAvailable,
-  required String word,
-  required String col,
-  required String location,
-  required String actualVerticalBeforeStartingLocationIfAvailable,
-  required String actualVerticalAfterEndingLocationIfAvailable,
-  required int rowInt,
-  required int colInt,
-  required Map<String, List<String>> letterPositions,
-  required String letter,
-  required int letterIndex,
-  required List<String> foundLocations,
-}) {
+bool canStartVertically(WordPlacementData data) {
   // This is the VERTICAL starting point of the word IF it can be placed
   final int actualVerticalRow =
-      actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
+      data.actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
 
   // Checking if there is enough space to the bottom & top
-  if (spaceFromTop < distanceFromTopOfLetter ||
-      spaceFromBottom < distanceFromBottomtOfLetter ||
+  if (data.spaceFromTop < data.distanceFromTopOfLetter ||
+      data.spaceFromBottom < data.distanceFromBottomOfLetter ||
       actualVerticalRow < 1) {
     return false;
   }
@@ -35,7 +18,7 @@ bool canStartVertically({
   final int verticalRowIterator = actualVerticalRow;
 
   final int actualStartingRowInt =
-      actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
+      data.actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
 
   bool hasActualConflicts = false;
 
@@ -43,61 +26,62 @@ bool canStartVertically({
   //   kLog.i('Looking for $letter in $location for vertical availability');
   // }
   // Iterating through the word to check if there are any conflicts
-  for (var k = 0; k < word.length; k++) {
+  for (var k = 0; k < data.word.length; k++) {
     // If we are at the end of the board, we break
     if (actualStartingRowInt + k > 10 ||
         verticalRowIterator + k - 1 >
-            actualVerticalEndingLocationIfAvailable.before('.').toInt()!) {
+            data.actualVerticalEndingLocationIfAvailable.before('.').toInt()!) {
       break;
     }
     // Main location to check
-    final String locationToCheck = '${verticalRowIterator + k}.$col';
+    final String locationToCheck = '${verticalRowIterator + k}.${data.colInt}';
 
     // Locations to check to the left and right
     final String beforeStartWordLetterLocation =
-        '${actualVerticalRow - distanceFromTopOfLetter}.$colInt';
+        '${actualVerticalRow - data.distanceFromTopOfLetter}.${data.colInt}';
     final String afterEndWordLetterLocation =
-        '${actualVerticalRow + distanceFromBottomtOfLetter}.$colInt';
+        '${actualVerticalRow + data.distanceFromBottomOfLetter}.${data.colInt}';
 
     final String leftLocationToCheck =
-        '${verticalRowIterator + k}.${colInt - 1}';
+        '${verticalRowIterator + k}.${data.colInt - 1}';
     final String leftBottomLocationToCheck =
-        '${verticalRowIterator + k + 1}.${colInt - 1}';
+        '${verticalRowIterator + k + 1}.${data.colInt - 1}';
     final String leftTopLocationToCheck =
-        '${verticalRowIterator + k - 1}.${colInt - 1}';
+        '${verticalRowIterator + k - 1}.${data.colInt - 1}';
 
     final String rightLocationToCheck =
-        '${verticalRowIterator + k}.${colInt + 1}';
+        '${verticalRowIterator + k}.${data.colInt + 1}';
     final String rightBottomLocationToCheck =
-        '${verticalRowIterator + k + 1}.${colInt + 1}';
+        '${verticalRowIterator + k + 1}.${data.colInt + 1}';
     final String rightTopLocationToCheck =
-        '${verticalRowIterator + k - 1}.${colInt + 1}';
+        '${verticalRowIterator + k - 1}.${data.colInt + 1}';
 
-    final String topLocationToCheck = '${verticalRowIterator + k - 1}.$colInt';
+    final String topLocationToCheck =
+        '${verticalRowIterator + k - 1}.${data.colInt}';
     final String bottomLocationToCheck =
-        '${verticalRowIterator + k + 1}.$colInt';
+        '${verticalRowIterator + k + 1}.${data.colInt}';
 
     final bool isRightBottomOutOfReach =
         rightBottomLocationToCheck.before('.').toInt()! >
-            actualVerticalEndingLocationIfAvailable.before('.').toInt()!;
+            data.actualVerticalEndingLocationIfAvailable.before('.').toInt()!;
     final bool isLeftBottomOutOfReach =
         leftBottomLocationToCheck.before('.').toInt()! >
-            actualVerticalEndingLocationIfAvailable.before('.').toInt()!;
+            data.actualVerticalEndingLocationIfAvailable.before('.').toInt()!;
 
     final bool isRightTopOutOfReach =
         rightTopLocationToCheck.before('.').toInt()! <
-            actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
+            data.actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
 
     final bool isLeftTopOutOfReach =
         leftTopLocationToCheck.before('.').toInt()! <
-            actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
+            data.actualVerticalStartingLocationIfAvailable.before('.').toInt()!;
 
     final hasConflicts = () {
       // This is the letter of the location we are checking
       // If it is empty, it means that there is no letter in the location
-      final String checkingLetter = word.charAt(k);
+      final String checkingLetter = data.word.charAt(k);
 
-      final String currentLocationLetter = letterPositions
+      final String currentLocationLetter = data.letterPositions
           .whereValue(
             (value) => value.contains(locationToCheck),
             orElse: () => {"": []},
@@ -109,136 +93,139 @@ bool canStartVertically({
       // If there is a letter in the location and it is
       // included in the word, we return true
       final bool isCurrentLetterPartOfTheWord =
-          word.charAt(k) == currentLocationLetter;
+          data.word.charAt(k) == currentLocationLetter;
 
       final bool currentLocationHasConflict =
-          (letterPositions.anyValue((v) => v.contains(locationToCheck)) &&
-                  locationToCheck != location) ||
+          (data.letterPositions.anyValue((v) => v.contains(locationToCheck)) &&
+                  locationToCheck != data.location) ||
               (currentLocationLetter.isEmpty &&
-                      letterPositions.anyValue(
+                      data.letterPositions.anyValue(
                         (value) => value.contains(rightLocationToCheck),
                       ) ||
                   currentLocationLetter.isEmpty &&
-                      letterPositions.anyValue(
+                      data.letterPositions.anyValue(
                         (value) => value.contains(leftLocationToCheck),
                       ));
 
-      final bool afterEndHasConflicts = letterPositions.anyValue(
-        (v) => v.contains(actualVerticalAfterEndingLocationIfAvailable),
+      final bool afterEndHasConflicts = data.letterPositions.anyValue(
+        (v) => v.contains(data.actualVerticalAfterEndingLocationIfAvailable),
       );
 
-      final bool beforeStartHasConflicts = letterPositions.anyValue(
-        (v) => v.contains(actualVerticalBeforeStartingLocationIfAvailable),
+      final bool beforeStartHasConflicts = data.letterPositions.anyValue(
+        (v) => v.contains(data.actualVerticalBeforeStartingLocationIfAvailable),
       );
 
-      final bool leftTopLocationHasConflict = (letterPositions.anyValue(
+      final bool leftTopLocationHasConflict = (data.letterPositions.anyValue(
                 (v) => v.contains(leftTopLocationToCheck),
               ) &&
-              actualVerticalStartingLocationIfAvailable.before('.').toInt()! <=
+              data.actualVerticalStartingLocationIfAvailable
+                      .before('.')
+                      .toInt()! <=
                   leftTopLocationToCheck.before('.').toInt()!) &&
-          letterPositions.anyValue(
+          data.letterPositions.anyValue(
             (v) => v.contains(leftLocationToCheck),
           );
 
-      final bool rightLocationHasConflict = (letterPositions.anyValue(
+      final bool rightLocationHasConflict = (data.letterPositions.anyValue(
                 (v) => v.contains(rightLocationToCheck),
               ) &&
-              letterPositions.anyValue(
+              data.letterPositions.anyValue(
                 (v) => v.contains(rightTopLocationToCheck),
               ) &&
               !isRightTopOutOfReach) ||
-          (letterPositions.anyValue(
+          (data.letterPositions.anyValue(
                 (v) => v.contains(rightLocationToCheck),
               ) &&
-              letterPositions.anyValue(
+              data.letterPositions.anyValue(
                 (v) => v.contains(rightBottomLocationToCheck),
               ) &&
               !isRightBottomOutOfReach) ||
           (!isCurrentLetterPartOfTheWord &&
-                  locationToCheck == actualVerticalEndingLocationIfAvailable) &&
-              letterPositions.anyValue(
+                  locationToCheck ==
+                      data.actualVerticalEndingLocationIfAvailable) &&
+              data.letterPositions.anyValue(
                 (v) => v.contains(rightLocationToCheck),
               ) ||
           checkingLetter.isEmpty &&
-              letterPositions.anyValue(
+              data.letterPositions.anyValue(
                 (v) => v.contains(rightLocationToCheck),
               );
 
-      final bool leftLocationHasConflict = (letterPositions.anyValue(
+      final bool leftLocationHasConflict = (data.letterPositions.anyValue(
                 (v) => v.contains(leftLocationToCheck),
               ) &&
-              letterPositions.anyValue(
+              data.letterPositions.anyValue(
                 (v) => v.contains(leftTopLocationToCheck),
               ) &&
               !isLeftTopOutOfReach) ||
-          (letterPositions.anyValue(
+          (data.letterPositions.anyValue(
                 (v) => v.contains(leftLocationToCheck),
               ) &&
-              letterPositions.anyValue(
+              data.letterPositions.anyValue(
                 (v) => v.contains(leftBottomLocationToCheck),
               ) &&
               !isLeftBottomOutOfReach) ||
           (!isCurrentLetterPartOfTheWord &&
                   locationToCheck ==
-                      actualVerticalStartingLocationIfAvailable) &&
-              letterPositions.anyValue(
+                      data.actualVerticalStartingLocationIfAvailable) &&
+              data.letterPositions.anyValue(
                 (v) => v.contains(leftLocationToCheck),
               ) ||
           checkingLetter.isEmpty &&
-              letterPositions.anyValue(
+              data.letterPositions.anyValue(
                 (v) => v.contains(leftLocationToCheck),
               );
 
-      if (word == 'ΖΕΑ' && actualVerticalStartingLocationIfAvailable == '1.8') {
+      if (data.word == 'ΖΕΑ' &&
+          data.actualVerticalStartingLocationIfAvailable == '1.8') {
         kLog.f('''
 Actual vertical row $actualVerticalRow
 
-Letter $letter letterIndex $letterIndex
+Letter ${data.letter} letterIndex ${data.letterIndex}
 
-First check ${letterPositions.anyValue(
+First check ${data.letterPositions.anyValue(
                   (v) => v.contains(leftLocationToCheck),
-                ) && letterPositions.anyValue(
+                ) && data.letterPositions.anyValue(
                   (v) => v.contains(leftTopLocationToCheck),
                 )}
 
-Second check ${letterPositions.anyValue(
+Second check ${data.letterPositions.anyValue(
                   (v) => v.contains(leftLocationToCheck),
-                ) && letterPositions.anyValue(
+                ) && data.letterPositions.anyValue(
                   (v) => v.contains(leftBottomLocationToCheck),
                 )}
-              
-What is this ${(!isCurrentLetterPartOfTheWord && locationToCheck == actualVerticalStartingLocationIfAvailable) && letterPositions.anyValue(
+
+What is this ${(!isCurrentLetterPartOfTheWord && locationToCheck == data.actualVerticalStartingLocationIfAvailable) && data.letterPositions.anyValue(
                   (v) => v.contains(leftLocationToCheck),
                 )}
 
-Or this ${checkingLetter.isEmpty && letterPositions.anyValue(
+Or this ${checkingLetter.isEmpty && data.letterPositions.anyValue(
                   (v) => v.contains(leftLocationToCheck),
                 )}
               
 Current letter part of the word $isCurrentLetterPartOfTheWord
 Checking letter is null ${checkingLetter.isEmpty}
 
-Left location has character ${letterPositions.anyValue((v) => v.contains(leftLocationToCheck))}
-Left top location has character ${letterPositions.anyValue((v) => v.contains(leftTopLocationToCheck))}
-Left bottom location has character ${letterPositions.anyValue((v) => v.contains(leftBottomLocationToCheck))}
+Left location has character ${data.letterPositions.anyValue((v) => v.contains(leftLocationToCheck))}
+Left top location has character ${data.letterPositions.anyValue((v) => v.contains(leftTopLocationToCheck))}
+Left bottom location has character ${data.letterPositions.anyValue((v) => v.contains(leftBottomLocationToCheck))}
 
-Right location has character ${letterPositions.anyValue((v) => v.contains(rightLocationToCheck))}
-Right top location has character ${letterPositions.anyValue((v) => v.contains(rightTopLocationToCheck))}
-Right bottom location has character ${letterPositions.anyValue((v) => v.contains(rightBottomLocationToCheck))}
+Right location has character ${data.letterPositions.anyValue((v) => v.contains(rightLocationToCheck))}
+Right top location has character ${data.letterPositions.anyValue((v) => v.contains(rightTopLocationToCheck))}
+Right bottom location has character ${data.letterPositions.anyValue((v) => v.contains(rightBottomLocationToCheck))}
 
 k is $k
-Letter is $letter
-Iterating over letter ${word.charAt(k)}
-word.charAt(k) != checkingLocationLetter ${word.charAt(k) != checkingLetter}
-Possible start $actualVerticalStartingLocationIfAvailable
-Possible end $actualVerticalEndingLocationIfAvailable
-Before start $beforeStartWordLetterLocation actual $actualVerticalBeforeStartingLocationIfAvailable
-After end $afterEndWordLetterLocation actual $actualVerticalAfterEndingLocationIfAvailable
+Letter is ${data.letter}
+Iterating over letter ${data.word.charAt(k)}
+data.word.charAt(k) != checkingLocationLetter ${data.word.charAt(k) != checkingLetter}
+Possible start ${data.actualVerticalStartingLocationIfAvailable}
+Possible end ${data.actualVerticalEndingLocationIfAvailable}
+Before start ${beforeStartWordLetterLocation} actual ${data.actualVerticalBeforeStartingLocationIfAvailable}
+After end ${afterEndWordLetterLocation} actual ${data.actualVerticalAfterEndingLocationIfAvailable}
 
-locationToCheck $locationToCheck
-locationToCheck letter $currentLocationLetter is it part of the word ? $isCurrentLetterPartOfTheWord
-location $location
-Intersection location $location
+locationToCheck ${locationToCheck}
+locationToCheck letter ${currentLocationLetter} is it part of the word ? ${isCurrentLetterPartOfTheWord}
+location ${data.location}
 
 
 --- Conflicts ---
@@ -258,23 +245,23 @@ Right location $rightLocationToCheck conflict $rightLocationHasConflict
 Right top location $rightTopLocationToCheck conflict - 
 Right bottom location $rightBottomLocationToCheck conflict rightBottomLocationHasConflict
 
-letterIndex$letterIndex != word.length${word.length} ${letterIndex != word.length}
+letterIndex${data.letterIndex} != word.length${data.word.length} ${data.letterIndex != data.word.length}
 
 Before start conflict $beforeStartHasConflicts
 After end conflict $afterEndHasConflicts
 Current location conflict $currentLocationHasConflict
 
 --- Conflicts ---
-Space from top $spaceFromTop
-Space from bottom $spaceFromBottom
-Distance from top of intersection $distanceFromTopOfLetter
-Distance from bottom $distanceFromBottomtOfLetter
+Space from top ${data.spaceFromTop}
+Space from bottom ${data.spaceFromBottom}
+Distance from top of intersection ${data.distanceFromTopOfLetter}
+Distance from bottom ${data.distanceFromBottomOfLetter}
 
 All letter positions:
-$letterPositions
+${data.letterPositions}
 
 Found locations
-$foundLocations
+${data.foundLocations}
 ''');
       }
       return currentLocationHasConflict ||
